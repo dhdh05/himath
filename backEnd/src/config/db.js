ca58124c2c4f9b4f29402aa0570f508177c3df9d
Huy Do <duchuyd241@gmail.com>
2025-12-30 01:51:02 +0700

    hhhhh

--- backEnd/src/config/db.js
+++ backEnd/src/config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'ktpmud',
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 1000,
     queueLimit: 0,
     charset: 'utf8mb4' // Hỗ trợ đầy đủ Unicode, bao gồm cả emoji
 });
