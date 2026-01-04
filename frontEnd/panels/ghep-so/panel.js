91fe194a25bcbdb6f10e82f8419de03df56a3177
PhanCongTrung <hanrung123456@gmail.com>
2026-01-04 22:10:00 +0700

    update

--- frontEnd/panels/ghep-so/panel.js
+++ frontEnd/panels/ghep-so/panel.js
@@ -36,12 +36,12 @@ export function mount(container) {
 
         <div class="game-main">
           <div class="numbers-section">
-            <h2><i class="fa-solid fa-hand-back-fist"></i> Kéo số từ đây</h2>
+            <h2>Kéo số từ đây <i class="fa-solid fa-arrow-right"></i></h2>
             <div class="numbers-container ghep-numbers-container"></div>
           </div>
 
           <div class="icons-section">
-            <h2><i class="fa-solid fa-hand"></i> Kéo sang đây với số lượng tương ứng</h2>
+            <h2><i class="fa-solid fa-arrow-right"></i> Kéo sang đây với số lượng tương ứng</h2>
             <div class="icons-container ghep-icons-container"></div>
           </div>
         </div>
